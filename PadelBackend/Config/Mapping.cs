fa9a3b15c22c5d98558cd9913b49f3d73e4f3e7c
LautaroLeonel <lautarofer456@gmail.com>
2024-06-20 17:59:56 -0300

    Comentar codigo del mapper para evitar conflicto de merge

--- PadelBackend/Config/Mapping.cs
+++ PadelBackend/Config/Mapping.cs
@@ -1,19 +1,19 @@
-﻿using AutoMapper;
-using PadelBackend.Models.Rackets;
-using PadelBackend.Models.Rackets.Dto;
+﻿//using AutoMapper;
+//using PadelBackend.Models.Rackets;
+//using PadelBackend.Models.Rackets.Dto;
 
-namespace PadelBackend.Config
-{
-    public class Mapping : Profile
-    {
-        public Mapping() 
-        { 
-            CreateMap<Racket, RacketDto>().ReverseMap();
-            CreateMap<Racket, RacketsDto>().ReverseMap();
-            CreateMap<CreateRacketDto, Racket>().ReverseMap();
-            //update sin nulls
-            CreateMap<UpdateRacketDto, RacketsDto>().ForAllMembers(opts => opts.Condition((_, _, srcMember) => srcMember != null));
-        }
+//namespace PadelBackend.Config
+//{
+//    public class Mapping : Profile
+//    {
+//        public Mapping() 
+//        { 
+//            CreateMap<Racket, RacketDto>().ReverseMap();
+//            CreateMap<Racket, RacketsDto>().ReverseMap();
+//            CreateMap<CreateRacketDto, Racket>().ReverseMap();
+//            //update sin nulls
+//            CreateMap<UpdateRacketDto, RacketsDto>().ForAllMembers(opts => opts.Condition((_, _, srcMember) => srcMember != null));
+//        }
 
-    }
-}
+//    }
+//}
