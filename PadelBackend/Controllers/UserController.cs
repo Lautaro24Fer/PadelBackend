44ddb4dc24e33e03f372760299e647ad8c5863e7
LautaroLeonel <lautarofer456@gmail.com>
2024-06-25 01:16:10 -0300

    v2) Documentacion de los endpoints relacionados a auth y usuarios

--- PadelBackend/Controllers/UserController.cs
+++ PadelBackend/Controllers/UserController.cs
@@ -18,20 +18,20 @@ namespace PadelBackend.Controllers
         {
             this.userServices = userServices;
         }
-        // SE ELIMINA PARA LA V2
         [HttpGet]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
         public async Task<ActionResult<List<UsersDto>>> Get()
         {
             try
             {
                 var users = await userServices.GetManyUsers();
-                return Ok(users);
+                return Ok(new {status = true, requestResponse = users, messageDetails = "The request was answered successfully" });
             }
             catch (Exception ex)
             {
-                return BadRequest(new { message = ex.Message });
+                return BadRequest(new { status = false, messageDetails = ex.Message });
             }
         }
         [HttpGet("{id:int}")]
@@ -46,33 +46,33 @@ namespace PadelBackend.Controllers
                 var user = await userServices.GetOneUser(id);
                 if (user == null)
                 {
-                    return NotFound();
+                    return NotFound(new { status = false, messageDetails = $"The user with '{id}' was not founded" });
                 }
-                return Ok(user);
+                return Ok(new { status = true, requestResponse = user, messageDetails = "The request was answered successfully" });
             }
             catch (Exception ex)
             {
-                return BadRequest(new { message = ex.Message });
+                return BadRequest(new { status = false, messageDetails = ex.Message });
             }
         }
         [HttpPost]
         [AllowAnonymous]
         [ProducesResponseType(StatusCodes.Status201Created)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
-        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
         public async Task<ActionResult<UserDto>> Post(CreateUserDto createUser)
         {
             if (ModelState.IsValid == false)
             {
-                return BadRequest(ModelState);
+                return BadRequest(new {status = false, requestResponse = ModelState, messageDetails = "Bad request. ModelState invalid"});
             }
             try
             {
-                return Ok(await userServices.CreateOneUser(createUser));
+                var user = await userServices.CreateOneUser(createUser);
+                return Ok(new { status = true, requestResponse = user, messageDetails = "The request was answered successfully" });
             }
             catch (Exception ex)
             {
-                return BadRequest(new { message = ex.Message });
+                return BadRequest(new { status = false, messageDetails = ex.Message });
             }
         }
         [HttpPut("{id:int}")]
@@ -82,25 +82,26 @@ namespace PadelBackend.Controllers
         public async Task<ActionResult<UserDto>> Put(int id, [FromBody] UpdateUserDto updateUser)
         {
             var idFromToken = User.FindFirst("id")?.Value;
-            if ((idFromToken == null) || (!int.TryParse(idFromToken, out int idParsed)))
+            if ((idFromToken == null) || (!int.TryParse(idFromToken, out _)))
             {
-                return Unauthorized(new {message = "Request denegated. Unauthorized"});
+                return Unauthorized(new {status = false, messageDetails = "Request denegated. Unauthorized"});
             }
             if(int.Parse(idFromToken) != id)
             {
-                return Forbid();
+                return Forbid("Can not update data from other user");
             }
             try
             {
-                return Ok(await userServices.UpdateOneUser(updateUser, id));
+                var user = await userServices.UpdateOneUser(updateUser, id);
+                return Ok(new { status = false, requestResponse = user, messageDetails = "The request was answered successfully" });
             }
             catch(NotFoundCustomEx ex)
             {
-                return NotFound(new { message = ex.errorMessageDetails ?? $"The user with id '{id}' was not founded"});
+                return NotFound(new { status = false, messageDetails = ex.errorMessageDetails ?? $"The user with id '{id}' was not founded"});
             }
             catch (Exception ex)
             {
-                return BadRequest(new { message = ex.Message });
+                return BadRequest(new { status = false, messageDetails = ex.Message });
             }
         }
     }   
